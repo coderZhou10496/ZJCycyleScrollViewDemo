8820d51557122356b408aab394564d6f23e277bc
JianZhou <ZJ@jianzhoudemacbook-pro.local>
2016-03-17 12:02:55 +0800

    Initial Commit

---
+++ ZJCycyleScrollViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZJCycyleScrollViewDemo
+//
+//  Created by ZJ on 16/3/17.
+//  Copyright © 2016年 ZJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
