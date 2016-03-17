02f6a5baf79376cd62a81b5f64f401e7d3f13d6f
JianZhou <1049654453@qq.com>
2016-03-17 14:20:57 +0800

    用UICollectionView实现的无限轮播图，可加载网络图片

--- ZJCycyleScrollViewDemo/ViewController.m
+++ ZJCycyleScrollViewDemo/ViewController.m
@@ -7,21 +7,49 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
+#import "ZJCycleScrollView.h"
+@interface ViewController ()<ZJCycleScrollViewDelegate>
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    
+    /*
+    //加载本地图片
+    NSArray *imageArray = @[[UIImage imageNamed:@"1.jpg"],
+                        [UIImage imageNamed:@"2.jpg"],
+                        [UIImage imageNamed:@"3.jpg"],
+                        ];
+    ZJCycleScrollView *scrollView = [[ZJCycleScrollView alloc] initWithFrame:CGRectMake(0, 100, self.view.bounds.size.width, 200) imagesGroup:imageArray];
+    scrollView.delegate = self;
+    scrollView.scrollTimeinterval = 3;
+    scrollView.showPageControl = YES;
+    scrollView.pageControlAlignment = ZJCycleScrollViewPageControlAlignmentCenter;
+    [self.view addSubview:scrollView];
+   */
+    
+    
+    //加载网络图片
+    NSArray *imagesURLStrings = @[
+                                  @"http://res1.gipower.cn/scb/ad/hailiao/banner.jpg",
+                                  @"http://res1.gipower.cn/scb/ad/bwjy/banner.jpg",
+                                  @"http://res1.gipower.cn/scb/ad/hycs/banner.jpg"
+                                  ];
+    
+    ZJCycleScrollView *scrollView = [[ZJCycleScrollView alloc] initWithFrame:CGRectMake(0, 100, self.view.bounds.size.width, 200) urlStringGroup:imagesURLStrings placeholderImage:[UIImage imageNamed:@"placeholder"]];
+    scrollView.delegate = self;
+    scrollView.scrollTimeinterval = 1;
+    scrollView.showPageControl = YES;
+    [self.view addSubview:scrollView];
+    
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)scrollView:(ZJCycleScrollView *)scrollView didSelectItemAtIndex:(int)index
+{
+    NSLog(@"你点击了第%d个图片",index);
 }
-
 @end
